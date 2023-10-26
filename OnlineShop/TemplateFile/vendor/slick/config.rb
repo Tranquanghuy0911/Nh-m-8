6e2f3ff7af1c76ab7ad35e607cf2a84bb4bd258f
Tranquanghuy0911 <huy091103@gmail.com>
2023-10-26 11:46:55 +0700

    init OnlineShop

---
+++ OnlineShop/TemplateFile/vendor/slick/config.rb
@@ -0,0 +1,10 @@
+css_dir = "."
+sass_dir = "."
+../TemplateFile/../TemplateFile/images_dir = "."
+fonts_dir = "fonts"
+relative_assets = true
+
+output_style = :compact
+line_comments = false
+
+preferred_syntax = :scss
---
+++ OnlineShop/Users/Site1.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineShop.Users
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "bg0 m-t-23 p-b-140");
+            }   
+            else
+            {
+                form1.Attributes.Remove("class");
+
+
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+
+                pnlSliderUC.Controls.Add(sliderUserControl); 
+            }    
+        }
+    }
+}
